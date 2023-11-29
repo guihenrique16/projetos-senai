a8710e3e1c2a24f18a6d0f4c3807d9f3fff99f9b
Guilherme Alberto <guilherme97035@gmail.com>
2023-11-29 17:08:51 -0300

    aula quinta

--- Event+/eventplus(js)/src/App.js
+++ Event+/eventplus(js)/src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+import { UserContext } from './context/AuthContext';
 import Rotas from './routes';
 
 function App() {
+  const [userData, setUserData] = useState(UserContext)
+
   return (
-    <Rotas />
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <Rotas />
+    </UserContext.Provider>
   );
 }
 
--- Event+/eventplus(js)/src/Pages/LoginPage/LoginPage.jsx
+++ Event+/eventplus(js)/src/Pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,104 @@
-import React from 'react';
-import Title from '../../Components/Title/Title';
+import React, { useContext, useState } from "react";
+import ImageIllustrator from "../../Components/ImageIllustrator/ImageIllustrator";
+import logo from "../../assets/images/logo-pink.svg";
+import { Input, Button } from "../../Components/FormComponents/FormComponents";
+import ImageLogin from'../../assets/images/login.svg'
+import api from'../../Services/Service'
+
+import "./LoginPage.css";
+import { UserContext, userDecodeToken } from "../../context/AuthContext";
 
 const LoginPage = () => {
-    return (
-        <div>
-            <Title titleText={"Página de Login"} />
+  const[user, setUser] = useState({email: "", senha:""})
+  const{userData,setUserData} = useContext(UserContext)
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(user);
+    if (user.email.length > 8 && user.senha.length >4 ) {
+        try {
+            const promise = await api.post("/Login",{
+
+                email: user.email,
+                senha: user.senha
+            })
+            console.log(promise.data.token);
+
+            const userFullToken = userDecodeToken(promise.data.token);
+            setUserData(userFullToken)
+        } catch (error) {
+            
+        }
+    } else{
+        alert("Email ou senha invalidos");
+    }
+  }
+
+  return (
+    <div className="layout-grid-login">
+      <div className="login">
+        <div className="login__illustration">
+          <div className="login__illustration-rotate"></div>
+          <ImageIllustrator
+            imageRender={ImageLogin}
+            altText="Imagem de um homem em frente de uma porta de entrada"
+            additionalClass="login-illustrator "
+          />
         </div>
-    );
+
+        <div className="frm-login">
+          <img src={logo} className="frm-login__logo" alt="" />
+
+          <form className="frm-login__formbox" 
+          onSubmit={handleSubmit}>
+            <Input
+              additionalClass="frm-login__entry"
+              type="email"
+              id="login"
+              name="login"
+              required={true}
+              value={user.email}
+              manipulationFunction={(e) => {
+                setUser({
+                    ...user,
+                    email: e.target.value.trim()
+                })
+              }}
+              placeholder="Username"
+              />
+
+            <Input
+              additionalClass="frm-login__entry"
+              type="password"
+              id="senha"
+              name="senha"
+              required={true}
+              value={user.senha}
+              manipulationFunction={(e) => {
+                setUser({
+                    ...user,
+                    senha: e.target.value.trim()
+                })
+              }}
+              placeholder="****"
+            />
+
+            <a href="" className="frm-login__link">
+              Esqueceu a senha?
+            </a>
+
+            <Button
+              textButton="Login"
+              id="btn-login"
+              name="btn-login"
+              type="submit"
+              additionalClass="frm-login__button"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
